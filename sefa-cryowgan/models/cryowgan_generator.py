5f116b35486dee88ca4cad61af53d74e5b65420f
OdyLiagkas <31992887+OdyLiagkas@users.noreply.github.com>
2024-11-20 12:11:27 +0200

    Updated cryowgan_generator.py to be 256x256

--- sefa-cryowgan/models/cryowgan_generator.py
+++ sefa-cryowgan/models/cryowgan_generator.py
@@ -6,9 +6,52 @@ from torch.autograd import Variable
 
 class Cryo_EM_Generator(nn.Module):
     """
-    NetG DCGAN Generator. Outputs 64x64 images.
+    NetG WGAN Generator. Outputs 256x256 images.
     """
-
+    def __init__(
+        self,
+        z_dim=100,
+        first_channel_size=256,
+        out_ch=3,
+        norm_layer=nn.BatchNorm2d,
+        final_activation=None,
+        
+    ):
+        super().__init__()
+        self.z_dim = z_dim
+        self.out_ch = out_ch
+        self.final_activation = final_activation
+        self.fcs = first_channel_size #first channel size
+        self.net = nn.Sequential(
+            # * Layer 1: 1x1
+            nn.ConvTranspose2d(self.z_dim, self.fcs, 4, 1, 0, bias=False),
+            norm_layer(self.fcs),
+            nn.ReLU(),
+            # * Layer 2: 4x4
+            nn.ConvTranspose2d(self.fcs, self.fcs//2, 4, 2, 1, bias=False),
+            norm_layer(self.fcs//2),
+            nn.ReLU(),
+            # * Layer 3: 8x8
+            nn.ConvTranspose2d(self.fcs//2, self.fcs//(2**2), 4, 2, 1, bias=False),
+            norm_layer(self.fcs//(2**2)),
+            nn.ReLU(),
+            # * Layer 4: 16x16
+            nn.ConvTranspose2d(self.fcs//(2**2), self.fcs//(2**3), 4, 2, 1, bias=False),
+            norm_layer(self.fcs//(2**3)),
+            nn.ReLU(),
+            # * Layer 5: 32x32
+            nn.ConvTranspose2d(self.fcs//(2**3), self.fcs//(2**4), 4, 2, 1, bias=False),
+            norm_layer(self.fcs//(2**4)),
+            nn.ReLU(),
+            # * Layer 6: 64x64
+            nn.ConvTranspose2d(self.fcs//(2**4), self.fcs//(2**5), 4, 2, 1, bias=False),
+            norm_layer(self.fcs//(2**5)),
+            nn.ReLU(),
+            # * Layer 7: 128x128
+            nn.ConvTranspose2d(self.fcs//(2**5), self.out_ch, 4, 2, 1, bias=False),
+            # * Output Layer 8: 256x256
+        )
+'''
     def __init__(
         self,
         z_dim=100,
@@ -49,7 +92,7 @@ class Cryo_EM_Generator(nn.Module):
         )
         ####################################################### Dictionary For Layers
         self.layer_dict = {f'layer{i}': layer for i, layer in enumerate(self.net) if isinstance(layer, nn.ConvTranspose2d)}
-
+'''
     
     def forward(self, x, synthesize=False):
         if synthesize:
